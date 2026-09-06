7ec2fdf8042f465a34ba8bfdf24e2fc3a3fccde6
Nicolaus Frauens <N.frauens@gmail.com>
2026-09-06 17:16:07 -0600

    recursive gen upate

--- src/generatepage.py
+++ src/generatepage.py
@@ -1,6 +1,7 @@
 import os
 from markdowntohtmlnode import markdown_to_html_node
 from splitnodedelimiter import extract_title
+import pathlib
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -12,3 +13,10 @@ def generate_page(from_path, template_path, dest_path):
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, mode="w") as f:
         f.write(new_page)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for i in os.listdir(dir_path_content):
+        if os.path.isfile(os.path.join(dir_path_content, i)):
+            generate_page(os.path.join(dir_path_content, i), template_path, pathlib.Path(dest_dir_path, "index.html"))
+            continue
+        generate_pages_recursive(os.path.join(dir_path_content, i), template_path, os.path.join(dest_dir_path, i))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 from textnode import TextNode, TextType
-from generatepage import generate_page
+from generatepage import generate_pages_recursive
 
 def recursive_copy(to_path: str, from_path: str):
     copy_to_path = to_path
@@ -19,6 +19,6 @@ def main():
     shutil.rmtree("./public", ignore_errors=True)
     os.mkdir("./public")
     recursive_copy("./public", "./static")
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content", "./template.html", "./public")
 
 main()
